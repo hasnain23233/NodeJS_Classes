26064741798e961bca7aee90206951374452202a
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-04 11:27:28 +0500

    adding new feature like edit home and delete home in airbnd website

--- Practice21/app.js
+++ Practice21/app.js
@@ -1,22 +1,35 @@
+//In this folder we make airbnd full beckend project using express.js and node js
 const express = require('express')
-const app = express()
-const rootPath = require('./utility/rootPath')
-const path = require('path')
 const bodyParser = require('body-parser')
+const userRoute = require('./routes/user/app')
+const { hostRoute } = require('./routes/host/hostRout')
+const pathUtils = require('./utility/pathUtils')
+const Error = require('./controllers/Error')
+const path = require('path')
 
-const userRouter = require('./routes/userRouter/user')
-const hostRouter = require('./routes/hostRouter/host')
 
-app.use(express.static(path.join(rootPath, 'public')))
+const app = express()
 
-app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(pathUtils, 'public')))
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(rootPath, 'views'))
+app.set('views', path.join(pathUtils, 'views'))
 
-app.use(userRouter)
-app.use(hostRouter.router)
+app.use('/', (req, res, next) => {
+    console.log(req.url, req.method)
+    next()
+})
+app.use(bodyParser.urlencoded())
+
+
+app.use(userRoute)
+app.use(hostRoute)
+
+app.use(Error.getEror404)
+
+
+
 
 app.listen(4200, () => {
-    console.log('Your server is running on the http://localhost:4200 In airbnd')
+    console.log('Your server is running on http://localhost:4200 Practice app')
 })
