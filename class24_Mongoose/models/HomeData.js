f5a75ed0e2c003cfa80c6e31dc5e328cf406a888
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-09 23:59:14 +0500

    creating the schema and delete the mongoDB database

--- class24_Mongoose/models/HomeData.js
+++ class24_Mongoose/models/HomeData.js
@@ -1,9 +1,9 @@
 
 const { ObjectId } = require('mongodb')
+const mongoose = require('mongoose')
 
-module.exports = class HomeData {
-    constructor(_id, name, email, home, img, description) {
-        if (_id) {
+/*
+if (_id) {
             this._id = _id;
         }
         this.name = name;
@@ -11,37 +11,16 @@ module.exports = class HomeData {
         this.home = home;
         this.img = img;
         this.description = description
-    }
+        save()  fetchingAll() findById(homeId) deleteById(homeId)
 
+*/
 
-    save() {
-        const db = getDB();
-        if (this._id) {
-            const updatedFields = {
-                name: this.name,
-                email: this.email,
-                home: this.home,
-                img: this.img,
-                description: this.description
-            }
-            return db.collection("homes").updateOne({ _id: new ObjectId(String(this._id)) }, { $set: updatedFields })
-        } else {
-            return db.collection("homes").insertOne(this)
-        }
-    }
+const Schema = new mongoose.Schema({
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    home: { type: String, require: true },
+    img: { type: String, require: true },
+    description: { type: String, require: true },
+})
 
-    static fetchingAll() {
-        const db = getDB()
-        return db.collection("homes").find().toArray()
-    }
-
-    static findById(homeId) {
-        const db = getDB()
-        return db.collection('homes').find({ _id: new ObjectId(String(homeId)) }).next()
-    }
-    static deleteById(homeId) {
-        const db = getDB()
-        return db.collection('homes').deleteOne({ _id: new ObjectId(String(homeId)) })
-    }
-
-}
+module.exports = Schema
