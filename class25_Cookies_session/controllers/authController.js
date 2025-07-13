478295c99365a4e39cc0aaaa8f03a63f089a0934
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-13 17:55:56 +0500

    adding the logout feature in the airbnd website

--- class25_Cookies_session/controllers/authController.js
+++ class25_Cookies_session/controllers/authController.js
@@ -4,10 +4,13 @@ exports.getLogin = (req, res, next) => {
     })
 }
 exports.postLogin = (req, res, next) => {
-    console.log(req.body)
     res.cookie('isLoggedIn', true, {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 // 1 hour
     })
     res.redirect('/')
+}
+exports.postLogout = (req, res, next) => {
+    res.cookie('isLoggedIn', false)
+    res.redirect('/login')
 }
