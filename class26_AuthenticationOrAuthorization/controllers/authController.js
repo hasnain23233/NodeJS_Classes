80bd74eb3766039a613c8e73927945b1bb99f1c3
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-07-20 14:51:58 +0500

    install express validator and add validation in postSignup funtion

--- class26_AuthenticationOrAuthorization/controllers/authController.js
+++ class26_AuthenticationOrAuthorization/controllers/authController.js
@@ -1,3 +1,5 @@
+const { check } = require("express-validator")
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         isLoggedIn: false
@@ -17,7 +19,60 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/login')
     })
 }
-exports.postsignup = (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/login')
-}
+exports.postsignup = [
+    check('firstName')
+        .trim()
+        .isLength({ min: 2 })
+        .withMessage('Your first name at lest 2 chractors ')
+        .matches(/^[A-Za-z\s]+S/)
+        .withMessage('First Name should only alphabets')
+    ,
+    check('lastName')
+        .matches(/^[A-Za-z\s]+S/)
+        .withMessage('Last Name should only alphabets')
+    ,
+    check('email')
+        .isEmail()
+        .withMessage('Please Enter a valid email')
+        .normalizeEmail()
+    ,
+    check('password')
+        .isLength({ min: 8 })
+        .withMessage('Your Pasword at lest 8 charactores')
+        .matches(/[a-z]/)
+        .withMessage('Your Password Should Lowercase Alphabets')
+        .matches(/[A-Z]/)
+        .withMessage('Your Password Should Uppercase Alphabets')
+        .matches(/[!@#$%^&*(),.?":{}|<>]/)
+        .withMessage('Your password must contain at least one special character')
+    ,
+    check('confirmPassword')
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password does not match')
+            }
+            return true
+        })
+    ,
+    check('userType')
+        .isEmpty()
+        .withMessage('use type was empty')
+        .isIn(['guest', 'host'])
+        .withMessage('Invalid user type')
+    ,
+    check('agree')
+        .isEmpty()
+        .withMessage('Please accept the terms and conditions')
+        .custom((value, { req }) => {
+            if (value !== 'on') {
+                throw new Error('Please accept the terms and conditions')
+            }
+            return true
+        })
+    ,
+    (req, res, next) => {
+        console.log(req.body)
+        res.redirect('/login')
+    }
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Practice26_AuthenticationOrAuthorization/views/**/*.ejs'],
+  content: ['./class26_AuthenticationOrAuthorization/views/**/*.ejs'],
   theme: {
     extend: {},
   },
