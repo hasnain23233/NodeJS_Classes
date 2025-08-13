6052e5f76e6666bc695d71a8841b9a59cbd9768f
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-13 23:56:43 +0500

    deleting files

--- class27_FileDownloadAndEdit/controllers/hostController.js
+++ class27_FileDownloadAndEdit/controllers/hostController.js
@@ -1,4 +1,8 @@
 const Home = require('../models/HomeData')
+const fs = require('fs')
+const path = require('path')
+const pathUtils = require('../utility/pathUtils')
+
 exports.getsHome = (req, res, next) => {
     res.render('host/editHome', {
         title: "Register Your Home",
@@ -66,17 +70,38 @@ exports.postAddHome = (req, res, next) => {
 
 
 // Edit Existing Home
+// Edit Existing Home
 exports.postEditHome = (req, res, next) => {
     const { id, name, email, home, description } = req.body;
 
     Home.findById(id).then((homes) => {
+        if (!homes) {
+            console.log("❌ Home not found for editing");
+            return res.redirect('/host_home_List');
+        }
+
         homes.name = name;
         homes.email = email;
         homes.home = home;
         homes.description = description;
 
         if (req.file) {
-            // Store relative path for browser
+            // Delete old image if it exists
+            if (homes.photo) {
+                // Extract old file name from stored URL
+                const oldFileName = homes.photo.split('/').pop();
+                const oldImagePath = path.join(pathUtils, 'upload', oldFileName);
+
+                fs.unlink(oldImagePath, (err) => {
+                    if (err) {
+                        console.log('⚠ Error deleting old image:', err.message);
+                    } else {
+                        console.log('🗑 Old image deleted successfully:', oldFileName);
+                    }
+                });
+            }
+
+            // Save new image path (relative URL for browser)
             homes.photo = '/class27_FileDownloadAndEdit/upload/' + req.file.filename;
         }
 
@@ -88,6 +113,7 @@ exports.postEditHome = (req, res, next) => {
         console.error("❌ Error editing home:", err);
     });
 };
+
 
 
 exports.postDeleteHome = (req, res, next) => {
